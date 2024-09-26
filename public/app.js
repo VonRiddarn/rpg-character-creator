4a765198722e0eb313b61a0f5fe1920799f75224
Alexander Bokedal <alexander.bokedal@gmail.com>
2024-09-26 13:32:30 +0200

    fixed items to be first letter uppercase and rest lowercase

--- public/app.js
+++ public/app.js
@@ -1,19 +1,16 @@
 // Pic enum
-const Racepicture = Object.freeze
-(
-	{
-		Human: "https://i.imgur.com/GV4Nvoj.jpeg",
-		Dwarf: "https://i.imgur.com/7nrEgXP.jpeg",
-		Elf: "https://i.imgur.com/Jt5y1Mc.jpeg",
-		Halfling: "https://i.imgur.com/uqiVPJK.jpeg",
-		Dragonborn: "https://i.imgur.com/i7oxXrY.jpeg",
-		Gnome: "https://i.imgur.com/6nkpCzo.jpeg",
-		Goliath: "https://i.imgur.com/E3fpDRL.jpeg",
-		Orc: "https://i.imgur.com/4k3FdXl.jpeg",
-		Aarakocra: "https://i.imgur.com/72kQpV0.jpeg",
-		Centaur: "https://i.imgur.com/7bCOdEU.jpeg",
-	}
-);
+const Racepicture = Object.freeze({
+  Human: "https://i.imgur.com/GV4Nvoj.jpeg",
+  Dwarf: "https://i.imgur.com/7nrEgXP.jpeg",
+  Elf: "https://i.imgur.com/Jt5y1Mc.jpeg",
+  Halfling: "https://i.imgur.com/uqiVPJK.jpeg",
+  Dragonborn: "https://i.imgur.com/i7oxXrY.jpeg",
+  Gnome: "https://i.imgur.com/6nkpCzo.jpeg",
+  Goliath: "https://i.imgur.com/E3fpDRL.jpeg",
+  Orc: "https://i.imgur.com/4k3FdXl.jpeg",
+  Aarakocra: "https://i.imgur.com/72kQpV0.jpeg",
+  Centaur: "https://i.imgur.com/7bCOdEU.jpeg",
+});
 
 const characters = [];
 
@@ -46,136 +43,123 @@ let saveCharacterButton = null;
 // Ensure variables are intiialized on page load
 window.onload = initializeElementVariables;
 
-function initializeElementVariables()
-{
-	// Persona
-	inputAlias = document.getElementById('input-name');
-	inputRace = document.getElementById('input-race');
-	inputAlignment = document.getElementsByName('radio-alignment');
-	
-	// Stats
-	inputStrength = document.getElementById('input-strength');
-	inputDexterity = document.getElementById('input-dexterity');
-	inputIntelligence = document.getElementById('input-intelligence');
-	inputWisdom = document.getElementById('input-wisdom');
-	inputCharisma = document.getElementById('input-charisma');
-	inputLuck = document.getElementById('input-luck');
-	
-	// Inventory
-	itemListContainer = document.getElementById('item-list-items-container');
+function initializeElementVariables() {
+  // Persona
+  inputAlias = document.getElementById("input-name");
+  inputRace = document.getElementById("input-race");
+  inputAlignment = document.getElementsByName("radio-alignment");
 
-	itemListHeader = document.getElementById('item-list-header');
-	itemListHeader.innerHTML = `ITEMS (${itemListContainer.childElementCount} / ${maxInventoryItems})`;
+  // Stats
+  inputStrength = document.getElementById("input-strength");
+  inputDexterity = document.getElementById("input-dexterity");
+  inputIntelligence = document.getElementById("input-intelligence");
+  inputWisdom = document.getElementById("input-wisdom");
+  inputCharisma = document.getElementById("input-charisma");
+  inputLuck = document.getElementById("input-luck");
 
+  // Inventory
+  itemListContainer = document.getElementById("item-list-items-container");
 
-	addItemInput = document.getElementById('text-add-item');
-	addItemButton = document.getElementById('button-add-item');
-	
-	// Functionality
-	header = document.getElementById('header');
-	saveCharacterButton = document.getElementById('button-save-character');
-	
-	initializeElementEvents();
+  itemListHeader = document.getElementById("item-list-header");
+  itemListHeader.innerHTML = `ITEMS (${itemListContainer.childElementCount} / ${maxInventoryItems})`;
+
+  addItemInput = document.getElementById("text-add-item");
+  addItemButton = document.getElementById("button-add-item");
+
+  // Functionality
+  header = document.getElementById("header");
+  saveCharacterButton = document.getElementById("button-save-character");
+
+  initializeElementEvents();
 }
 
-function initializeElementEvents()
-{
-	addItemButton.addEventListener('click', function (e)
-	{
-		if (!addItemInput.value || itemListContainer.childElementCount >= maxInventoryItems)
-			return;
+function initializeElementEvents() {
+  addItemButton.addEventListener("click", function (e) {
+    if (
+      !addItemInput.value ||
+      itemListContainer.childElementCount >= maxInventoryItems
+    )
+      return;
 
-		addItemToList(addItemInput.value);
-	});
-	
-	saveCharacterButton.addEventListener('click', function(e)
-	{
-		saveCharacter();
-		for (const element of characters) {
-			console.log(element);
-		}
-	});
-	
-	window.addEventListener('keydown',
-		function (e)
-		{
-			if (e.key == 'U+000A' || e.key == 'Enter' || e.code == 13)
-				e.preventDefault();
-		}
-	)
+    addItemToList(addItemInput.value);
+  });
+
+  saveCharacterButton.addEventListener("click", function (e) {
+    saveCharacter();
+    for (const element of characters) {
+      console.log(element);
+    }
+  });
+
+  window.addEventListener("keydown", function (e) {
+    if (e.key == "U+000A" || e.key == "Enter" || e.code == 13)
+      e.preventDefault();
+  });
 }
 
-function addItemToList(item)
-{
-	itemListContainer.innerHTML += `<li>${item}</li>`;
-	addItemInput.value = "";
-	addItemInput.focus();
-	itemListHeader.innerHTML = `ITEMS (${itemListContainer.childElementCount} / ${maxInventoryItems})`;
+function addItemToList(item) {
+  lowerCaseItem = item.toLowerCase();
+  firstLetterUpperCaseItem =
+    lowerCaseItem.charAt(0).toUpperCase() + lowerCaseItem.slice(1);
+  itemListContainer.innerHTML += `<li>${firstLetterUpperCaseItem}</li>`;
+  addItemInput.value = "";
+  addItemInput.focus();
+  itemListHeader.innerHTML = `ITEMS (${itemListContainer.childElementCount} / ${maxInventoryItems})`;
 }
 
-function saveCharacter()
-{
-	let av = null;
+function saveCharacter() {
+  let av = null;
 
-	for (const a of inputAlignment) 
-	{
-		if(!a.checked)
-			continue;
+  for (const a of inputAlignment) {
+    if (!a.checked) continue;
 
-		av = a.value;
-	}
+    av = a.value;
+  }
 
-	characters.push
-	(
-		{
-			alias: inputAlias.value,
-			race: inputRace.value,
-			alignment: av,
-			strength: inputStrength.value,
-			dexterity: inputDexterity.value,
-			intelligence: inputIntelligence.value,
-			wisdom: inputWisdom.value,
-			charisma: inputCharisma.value,
-			luck: inputLuck.value,
-			items: itemListContainer.innerHTML,
-		},
-	);
-	
-	updateHeader();
+  characters.push({
+    alias: inputAlias.value,
+    race: inputRace.value,
+    alignment: av,
+    strength: inputStrength.value,
+    dexterity: inputDexterity.value,
+    intelligence: inputIntelligence.value,
+    wisdom: inputWisdom.value,
+    charisma: inputCharisma.value,
+    luck: inputLuck.value,
+    items: itemListContainer.innerHTML,
+  });
+
+  updateHeader();
 }
 
-function getLinkFromRace(race)
-{
-	switch(race)
-	{
-		case "Human":
-		return Racepicture.Human;
-		case "Dwarf":
-		return Racepicture.Dwarf;
-		case "Elf":
-		return Racepicture.Elf;
-		case "Dragonborn":
-		return Racepicture.Dragonborn;
-		case "Halfling":
-		return Racepicture.Halfling;
-		case "Goliath":
-		return Racepicture.Goliath;
-		case "Gnome":
-		return Racepicture.Gnome;
-		case "Aarakocra":
-		return Racepicture.Aarakocra;
-		case "Orc":
-		return Racepicture.Orc;
-		case "Centaur":
-		return Racepicture.Centaur;
-	}
+function getLinkFromRace(race) {
+  switch (race) {
+    case "Human":
+      return Racepicture.Human;
+    case "Dwarf":
+      return Racepicture.Dwarf;
+    case "Elf":
+      return Racepicture.Elf;
+    case "Dragonborn":
+      return Racepicture.Dragonborn;
+    case "Halfling":
+      return Racepicture.Halfling;
+    case "Goliath":
+      return Racepicture.Goliath;
+    case "Gnome":
+      return Racepicture.Gnome;
+    case "Aarakocra":
+      return Racepicture.Aarakocra;
+    case "Orc":
+      return Racepicture.Orc;
+    case "Centaur":
+      return Racepicture.Centaur;
+  }
 }
 
-function updateHeader()
-{
-	let c = characters[characters.length-1];
-	header.innerHTML +=
-	`
+function updateHeader() {
+  let c = characters[characters.length - 1];
+  header.innerHTML += `
 		<section class="flashcard">
 			<img src=${getLinkFromRace(c.race)} alt=${c.race}>
 			<span>
